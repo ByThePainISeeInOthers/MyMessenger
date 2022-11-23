3363a7345cd78850277614685494d33b0bb13090
ByThePainISeeInOthers <alexander29072003@gmail.com>
2022-11-24 00:01:00 +0400

    add console client

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -5,15 +5,44 @@ namespace MyMessenger
 {
     class Program
     {
+        public static int MessageID;
+        private static string Username;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message("define", "Privet", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
+            //Message msg = new Message("define", "Privet", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
             //{ "UserName":"define","MessageText":"Privet","TimeStamp":"2022-11-22T17:32:56.4338551Z"}
             //define < 22.11.2022 17:32:56 >: Privet
+
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            Username = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                MessageText = Console.ReadLine();
+                
+                if (MessageText.Length > 0)
+                {
+                    Message SendMSG = new Message(Username, MessageText, DateTime.Now);
+                    API.SendMessage(SendMSG);
+                }
+                GetNewMessages();
+            }
         }
     }
 }
