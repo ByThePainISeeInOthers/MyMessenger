bfe6e3bf50c79fcd79e170f121da06843fa69252
ByThePainISeeInOthers <alexander29072003@gmail.com>
2022-11-25 00:40:17 +0400

    create WF Client

---
+++ WindowsFormsClient/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace WindowsFormsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbMessages = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(674, 383);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(114, 55);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lbMessages
+            // 
+            this.lbMessages.FormattingEnabled = true;
+            this.lbMessages.Location = new System.Drawing.Point(12, 12);
+            this.lbMessages.Name = "lbMessages";
+            this.lbMessages.Size = new System.Drawing.Size(776, 368);
+            this.lbMessages.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 386);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(365, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 418);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(656, 20);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lbMessages);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lbMessages;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ WindowsFormsClient/Form1.cs
@@ -0,0 +1,50 @@
+﻿using MyMessenger;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private static int MessageID = 0;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string Username = txtName.Text;
+            string Message = txtMessage.Text;
+            if ((Username.Length > 0) && (Message.Length > 0))
+            {
+                MyMessenger.Message msg = new MyMessenger.Message(Username, Message, DateTime.Now);
+                API.SendMessage(msg);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var getMessage = new Func<Task>(async () =>
+            {
+                MyMessenger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+                while (msg != null)
+                {
+                    lbMessages.Items.Add(msg);
+                    MessageID++;
+                    msg = await API.GetMessageHTTPAsync(MessageID);
+                }
+            });
+            getMessage.Invoke();
+        }
+    }
+}
